864f9d88b7e7ebb324cb36ad28ecd554950cc3ea
garrett04 <garrettcorreia21@gmail.com>
2023-12-13 17:43:21 +0000

    Update DarkModeToggle

--- src/components/Header/DarkModeToggle/DarkModeToggle.js
+++ src/components/Header/DarkModeToggle/DarkModeToggle.js
@@ -34,7 +34,7 @@ const LightDarkTheme = () => {
     }
 
     return (
-       <> 
+       <div className='box'> 
             <button 
                 id="dark-mode-toggle" 
                 className="dark-mode-toggle" 
@@ -44,7 +44,7 @@ const LightDarkTheme = () => {
                     <path fill="currentColor" d="M8,256C8,393,119,504,256,504S504,393,504,256,393,8,256,8,8,119,8,256ZM256,440V72a184,184,0,0,1,0,368Z" transform="translate(-8 -8)"/>
                 </svg>
             </button>
-        </>
+        </div>
     );
 }
 
