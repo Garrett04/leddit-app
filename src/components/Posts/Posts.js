061eb860a4d193726cb7322cca2d6cf34e453ea6
garrett04 <garrettcorreia21@gmail.com>
2023-12-16 00:01:43 +0000

    Update index.js, Posts.js Fix multiple fetch requests instead of one

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,33 +1,33 @@
-import Post from "./Post/Post";
 import { useEffect } from "react";
 // import { likePost } from "../../features/posts/postsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { getPostsError, getPostsStatus, selectAllPosts } from "../../features/posts/postsSlice";
 import { fetchSubredditData } from "../../data/redditData";
 import { LoadingSpinner } from "./LoadingSpinner";
+import Post from "./Post/Post";
 
 const Posts = () => {
     const dispatch = useDispatch();
     
     const posts = useSelector(selectAllPosts);
-    const status = useSelector(getPostsStatus);
+    const postsStatus = useSelector(getPostsStatus);
     const error = useSelector(getPostsError);
 
     useEffect(() => {
-        if (status === 'idle') {
-            dispatch(fetchSubredditData());
+        if (postsStatus === 'idle') {
+            dispatch(fetchSubredditData())
         }
-    }, [status, dispatch]);
+    }, [postsStatus, dispatch]);
 
     let cardContent;
 
-    if (status === 'pending') {
+    if (postsStatus === 'pending') {
         cardContent = <LoadingSpinner />;
-    } else if (status === 'fulfilled') {
+    } else if (postsStatus === 'fulfilled') {
         console.log(posts);
         cardContent = posts.map(post => <Post key={post.id} post={post} />)
-    } else {
-        cardContent = <p>{error}</p>
+    } else if (postsStatus === 'rejected') {
+        cardContent = <p>{error}</p>;
     }
 
     return (
