b8d232eb794f7da7bd1bdf8a80cfc436ce69c685
garrett04 <garrettcorreia21@gmail.com>
2023-12-20 15:20:28 +0000

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,20 @@
 import { useDarkMode } from "../DarkModeProvider";
 import DarkModeToggle from "./DarkModeToggle/DarkModeToggle";
 import SearchBar from "./SearchBar";
-
+import Drawer from '@mui/material/Drawer';
 import ledditLogo from '../../assets/images/leddit-logo.svg';
 
 const Header = () => {
     const { darkMode, toggleDarkMode } = useDarkMode();
+    
+    const handleClick = () => {
+      const subredditNav = document.querySelector('aside');
+      if (subredditNav.style.display === 'flex') {
+        subredditNav.style.display = 'none';
+      } else {
+        subredditNav.style.display = 'flex';
+      }
+    }
 
     return (
         <div className={ darkMode ? 'darkMode-header': 'header'}>
@@ -19,6 +28,7 @@ const Header = () => {
                 <SearchBar />
             <div className="left">
                 <DarkModeToggle toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
+                <button className='subredditNavBtn' onClick={handleClick}>SubNav</button>
             </div>
             </div>
         </div>
