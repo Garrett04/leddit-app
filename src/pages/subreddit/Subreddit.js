b5cc265f72d3180c5ee6372dbdfe4be1c61d943b
garrett04 <garrettcorreia21@gmail.com>
2023-12-21 16:41:41 +0000

    Update redditData.js, Subreddit.js Fix display something if subreddit not found

--- src/pages/subreddit/Subreddit.js
+++ src/pages/subreddit/Subreddit.js
@@ -2,40 +2,42 @@ import './Subreddit.css';
 import { useSelector } from "react-redux";
 import { selectAllSubreddits } from "../../features/subredditData/subredditsSlice";
 import { useParams } from "react-router-dom";
-import { selectAllSubredditPosts } from "../../features/subredditPosts/subredditPostsSlice";
 import Main from "../../components/main/Main";
 import Markdown from 'react-markdown';
 
 
 const Subreddit = () => {
-  const subredditPosts = useSelector(selectAllSubredditPosts);
   const subreddits = useSelector(selectAllSubreddits);
 
   const { subreddit } = useParams();
 
   // console.log(subreddit);
 
-  const renderSubreddit = () => (
-    subreddits.map(({
-      banner_img,
-      display_name,
-      id,
-      title,
-    }) => {
-      if (display_name === subreddit) {
-        return (
-          <div 
-            key={id} 
-            className="subredditHeading"
-          >
-              {banner_img ? <img src={banner_img}/> : null}
-              <h2>r/{display_name}</h2>
-              <Markdown>{title}</Markdown>
-          </div>
-        )
-      }
-    })
-  )
+  const renderSubreddit = () => {
+    const foundSubreddit = subreddits.find((subredditName) => subredditName.display_name === subreddit);
+
+    if (foundSubreddit) {
+      const { banner_img, display_name, id, title } = foundSubreddit;
+      return (
+        <div 
+          key={id} 
+          className="subredditHeading"
+        >
+            {banner_img ? <img src={banner_img}/> : null}
+            <h2>r/{display_name}</h2>
+            <Markdown>{title}</Markdown>
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className='subredditHeading'
+        >
+          <h2>r/{subreddit}</h2>
+        </div>
+      );
+    }
+  }
 
   // console.log(subredditPosts);
   // console.log(subreddits);
